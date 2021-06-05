baeaf37e6bf068f6d915fa585228c1bd4dd5c148
ClaireBassem <85247869+ClaireBassem@users.noreply.github.com>
2021-06-05 03:21:19 +0200

    Add files via upload
    Claire uploaded this file

---
+++ delay.c
@@ -0,0 +1,16 @@
+void delay_ms(int n)
+{
+		 int i,j;
+		 for(i=0;i<n;i++)
+		 for(j=0;j<3180;j++)
+		 {}
+}
+
+/* Micro seconds delay function */
+void delay_us(int n)
+{
+		 int i,j;
+		 for(i=0;i<n;i++)
+		 for(j=0;j<3;j++)
+		 {}
+ }
