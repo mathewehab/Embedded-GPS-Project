b4a277950d9095bc353957b559c031bda335194b
Selvia Mamdouh <85055958+3124Sel@users.noreply.github.com>
2021-06-05 03:41:57 +0200

    selvia uploaded this file

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "code.c"
+
+int main() {   
+
+    
+            FPUEnable();
+						FPULazyStackingEnable();
+						RGBLED_Init();
+//					SevenSeg_Data(578,5);
+//					delay_ms(500);
+//					SevenSeg_Data(123,10);
+						
+						
+	
+						dist = CalcGPSDistance(30.053716529290657, 31.255879108910797,30.05336944148598, 31.255492772461782); //small
+						TestDistance(dist);
+						SevenSeg_Data(dist,10);
+						delay_ms(500);
+	
+						dist = CalcGPSDistance(30.053716529290657, 31.255879108910797,30.052805288432584, 31.254816855794772); //Large
+						TestDistance(dist);
+						SevenSeg_Data(dist,10);
+						delay_ms(500);
+					
+						SevenSeg_Data(1000,10);
+
+}
