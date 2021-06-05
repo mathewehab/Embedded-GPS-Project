89706ce9a181c37aa0db9d81e3490f393e01abce
Selvia Mamdouh <85055958+3124Sel@users.noreply.github.com>
2021-06-05 13:14:43 +0200

    selvia uploaded this file

--- uart_intialization.c
+++ uart_intialization.c
@@ -10,14 +10,14 @@ UART2_CTL_R &=~0x0001; //diable uart uartctl
 //in uart0 use it as alternate function afsel
 //pctl of uart
 
-UART2_IBRD_R = 0x0208;
-UART2_FBRD_R = 0x35;//baudrate
-UART2_LCRH_R = 0x0070;//uartlcrh: data lengh 8bit bit5,bit6 ==1 --one stop bit bit3 ==0 -- no parity bit1,bit2 ==0 --enable fifo bit4 ==1
-UART2_CTL_R |= 0x0001;//enable uartctl
+UART2_IBRD_R = 0x0208;    //or 104 
+UART2_FBRD_R = 0x35 ;   //baudrate
+UART2_LCRH_R = 0x0070;      //uartlcrh: data lengh 8bit bit5,bit6 ==1 --one stop bit bit3 ==0 -- no parity bit1,bit2 ==0 --enable fifo bit4 ==1
+UART2_CTL_R |= 0x0001;         //enable uartctl
 
 GPIO_PORTD_AFSEL_R |= 0xC0;
 GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R &0x00FFFFFF)+0x11000000 ;
-GPIO_PORTD_DEN_R |= 0xC0;
-GPIO_PORTD_AMSEL_R &=~ 0XC0;
+GPIO_PORTD_DEN_R |= 0xC0;      //digital enable
+GPIO_PORTD_AMSEL_R &=~ 0XC0;       //analog
 
 }
