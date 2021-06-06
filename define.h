7a3c1c9c870da7336d0193aca9a5467cb1ecefa4
mathewehab <71157340+mathewehab@users.noreply.github.com>
2021-06-06 13:50:01 +0200

    Add files via upload

---
+++ define.h
@@ -0,0 +1,83 @@
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+
+
+#define GPIO_PORTB_DATA_R       (*((volatile unsigned long *)0x400053FC))
+#define GPIO_PORTB_DIR_R        (*((volatile unsigned long *)0x40005400))
+#define GPIO_PORTB_AFSEL_R      (*((volatile unsigned long *)0x40005420))
+#define GPIO_PORTB_PUR_R        (*((volatile unsigned long *)0x40005510))
+#define GPIO_PORTB_DEN_R        (*((volatile unsigned long *)0x4000551C))
+#define GPIO_PORTB_LOCK_R       (*((volatile unsigned long *)0x40005520))
+#define GPIO_PORTB_CR_R         (*((volatile unsigned long *)0x40005524))
+#define GPIO_PORTB_AMSEL_R      (*((volatile unsigned long *)0x40005528))
+#define GPIO_PORTB_PCTL_R       (*((volatile unsigned long *)0x4000552C))
+
+
+#define PF4                     (*((volatile unsigned long *)0x40025040))
+#define PF3                     (*((volatile unsigned long *)0x40025020))
+#define PF2                     (*((volatile unsigned long *)0x40025010))
+#define PF1                     (*((volatile unsigned long *)0x40025008))
+#define PF0                     (*((volatile unsigned long *)0x40025004))
+	
+#define PB1                     (*((volatile unsigned long *)0x40025008))
+#define PB0                     (*((volatile unsigned long *)0x40025004))
+	
+#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
+#define SYSCTL_RCGCGPIO_R       (*((volatile unsigned long *)0x400FE608))
+#define SYSCTL_PRGPIO_R         (*((volatile unsigned long *)0x400FEA08))
+	
+#define RED       0x02
+#define BLUE      0x04
+#define GREEN     0x08
+
+#define SW1 			0x10
+#define SW2 			0x01
+#define RGB 			0x0E
+
+#define PF123_mask             0x0E
+#define PF04_mask              0x11
+#define PF_mask                0x20
+#define PB_mask 							 0x02
+#define PI 3.14159265358979323846
+#define EarthRadius 6372797.56085
+#define DegreeToRadiant PI / 180
+
+#define ClearLCD 0x01
+#define ForceToBegining 0x80
+#define IncrementLCD 0x06
+
+#define LCD GPIOB   /* Define "LCD" as a symbolic name for GPIOB */
+#define RS 0x20 /* PORTB BIT5 mask */
+#define RW 0x40 /* PORTB BIT6 mask */
+#define EN 0x80 /* PORTB BIT7 mask */
+#define HIGH 1
+#define LOW 0
+
+/*define useful symbolic names for LCD commands */
+#define clear_display     0x01
+#define returnHome        0x02
+#define moveCursorRight   0x06
+#define moveCursorLeft    0x08
+#define shiftDisplayRight 0x1C
+#define shiftDisplayLeft  0x18
+#define cursorBlink       0x0F
+#define cursorOff         0x0C
+#define cursorOn          0x0E
+#define Function_set_4bit 0x28
+#define Function_set_8bit 0x38
+#define Entry_mode        0x06
+#define Function_8_bit    0x32
+#define Set5x7FontSize    0x20
+#define FirstRow          0x80
+
+//7 segment display
+#define digit1 0x10
+#define digit2 0x20
+#define digit3 0x40
