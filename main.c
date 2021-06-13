524f1f28a54584acf7e34e786e35f5ff4c00aec4
ClaireBassem <85247869+ClaireBassem@users.noreply.github.com>
2021-06-13 16:38:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,36 @@
 #include <string.h>
 #include <stdlib.h>
 
-
+unsigned char Gpsdata;             // for incoming serial data
+unsigned int finish =0;            // indicate end of message
+unsigned int pos_cnt=0;            // position counter
+unsigned int lat_cnt=0;            // latitude data counter
+unsigned int log_cnt=0;            // longitude data counter
+unsigned int flg    =0;            // GPS flag
+unsigned int com_cnt=0;            // comma counter
+char lat[20];             // latitude array
+char lg[20];              // longitude array
+char distance_Display[20];
+char total_distance_Display[20];
+unsigned int i=0;
+unsigned int fg=0;
+double lat_raw;
+double lg_raw;
+unsigned int degrees_lat;
+unsigned int minutes_lat;
+double seconds_lat;
+unsigned int degrees_lon;
+unsigned int minutes_lon;
+double seconds_lon;
+double Latitude;
+double Longitude;
+double Total_distance=0.0;
+char lat_new_display[20]; // display variables for testing on serial port
+char long_new_display[20];
+char lat_old_display[20];
+char long_old_display[20];
+char lat_raw_display[20];
+char long_raw_display[20];
 
 void RGBLED_Init(void);
 void delay_ms(int);
