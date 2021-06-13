41a1eb6b2e657c1f3374169043ca85953db794c7
ClaireBassem <85247869+ClaireBassem@users.noreply.github.com>
2021-06-13 16:32:47 +0200

    Update gps_uart_intialization.c

--- gps_uart_intialization.c
+++ gps_uart_intialization.c
@@ -6,18 +6,23 @@
 void UART_Init(void){
 SYSCTL_RCGCUART_R |=0x0004; //enable uart4 and Port D
 SYSCTL_RCGCGPIO_R |=0x0008;
-UART2_CTL_R &=~0x0001; //diable uart uartctl
+UART2_CTL_R  &=~0x0001;    //diable uart uartctl
 //in uart0 use it as alternate function afsel
 //pctl of uart
 
-UART2_IBRD_R = 104;    //or 104 
+UART2_IBRD_R = 104;     
 UART2_FBRD_R = 11 ;   //baudrate
 UART2_LCRH_R = 0x0070;      //uartlcrh: data lengh 8bit bit5,bit6 ==1 --one stop bit bit3 ==0 -- no parity bit1,bit2 ==0 --enable fifo bit4 ==1
-UART2_CTL_R |= 0x0001;         //enable uartctl
+UART2_CTL_R |= 0x0301;         //enable uartctl
 
 GPIO_PORTD_AFSEL_R |= 0xC0;
 GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R &0x00FFFFFF)+0x11000000 ;
 GPIO_PORTD_DEN_R |= 0xC0;      //digital enable
 GPIO_PORTD_AMSEL_R &=~ 0XC0;       //analog
 
+}
+
+char UART_Inchar(void){         // uart character input
+	while((UART2_FR_R &0X10)!=0);
+	return(char)(UART2_DR_R & 0xFF);
 }
