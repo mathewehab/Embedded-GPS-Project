0376c5a310e98dc48d0af14340c73e8650333d5c
ClaireBassem <85247869+ClaireBassem@users.noreply.github.com>
2021-06-13 16:27:00 +0200

    Update Startup.c

--- Startup.c
+++ Startup.c
@@ -4,10 +4,18 @@
 #include <stdint.h>
 
 void RGBLED_Init(void);
+void UART_Init(void);
+void SevenSeg_Init(void);
+void UART0_Init(void);
+
 
 
 void Startup(void){
 		     FPUEnable();
 				 FPULazyStackingEnable();
 				 RGBLED_Init();
+				 UART_Init();
+				 SevenSeg_Init();
+				 UART0_Init();
+
 }
